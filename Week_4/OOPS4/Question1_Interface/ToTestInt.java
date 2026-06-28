2223eab04f24961f3c29e695e58b5d414bb4b042
Sakshiiii-codes <singhsakshi9197@gmail.com>
2026-06-28 22:22:24 +0530

    Create ToTestInt.java

---
+++ Week_4/OOPS4/Question1_Interface/ToTestInt.java
@@ -0,0 +1,20 @@
+interface test {
+    void square(int n);
+}
+
+class Arithmetic implements test {
+
+    public void square(int n) {
+        System.out.println("Square = " + (n * n));
+    }
+}
+
+public class ToTestInt {
+
+    public static void main(String[] args) {
+
+        Arithmetic obj = new Arithmetic();
+        obj.square(5);
+
+    }
+}
